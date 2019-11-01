439d799c2d6eb7d87dfa16d0719b46f8f636f664
michaelpeoples <42450795+michaelpeoples@users.noreply.github.com>
2019-10-31 23:25:24 -0500

    Updated Doors

---
+++ Assets/Open.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open : MonoBehaviour
+{
+
+    private SphereCollider _enterTrigger;
+    public float RotationRate;
+    public float TimeBetweenRotations;
+    public float MaxRotation;
+    public float Timer;
+    public bool InRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enterTrigger = gameObject.GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (InRange && (gameObject.transform.rotation.y < MaxRotation))
+        {
+            Timer += Time.deltaTime;
+            if (Timer >= TimeBetweenRotations)
+            {
+                gameObject.transform.Rotate(new Vector3(0, RotationRate));
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        InRange = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        InRange = false;
+    }
+}
