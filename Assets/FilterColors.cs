af713981b34b5fefa5adf67a54508e449ba31d10
michaelpeoples <42450795+michaelpeoples@users.noreply.github.com>
2019-10-31 20:11:19 -0500

    Added Castle Pieces and Snow Scene
    Snow Scene

---
+++ Assets/FilterColors.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilterColors : MonoBehaviour
+{
+   public Color StartColor;
+   public Color EndColor;
+   public float TransitionTime;
+   Light Light;
+
+    private void Start()
+    {
+        Light = gameObject.GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        Light.color = Color.Lerp(StartColor, EndColor, Mathf.PingPong(Time.time, TransitionTime));
+    }
+}
